fa57e46fbe9a2a4af875a1a59b6c647962d49f79
Basal Ali <basalali@hotmail.com>
2019-09-30 14:25:34 -0400

    Part 1

---
+++ 11_Inheritance/pair-exercise/BankTellerExercise/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankTellerExercise
+{
+   public class BankAccount
+    {
+        public BankAccount()
+        {
+            Balance = 0M;
+        }
+
+        public string AccountNumber
+        {
+            get { return AccountNumber; }
+            set { }
+        }
+        public decimal Balance { get; protected set; } = 0;
+        // balance begins at 0
+       
+
+
+        public decimal Deposit(decimal amountToDeposit)
+        {
+            Balance = amountToDeposit + Balance;
+            return Balance;
+        }
+
+        public virtual decimal Withdraw(decimal amountToWithdraw)
+        {
+            Balance = Balance - amountToWithdraw;
+            return Balance;
+        }
+
+
+        public void Transfer(BankAccount destinationAccount, decimal transferAmount)
+        {
+            destinationAccount.Balance = destinationAccount.Balance + transferAmount;
+            Balance = Balance - transferAmount;
+        }
+
+
+
+    }
+}
--- 11_Inheritance/pair-exercise/BankTellerExercise/Program.cs
+++ 11_Inheritance/pair-exercise/BankTellerExercise/Program.cs
@@ -6,7 +6,34 @@ namespace BankTellerExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount checkingAccount = new CheckingAccount();
+            BankAccount savingsAccount = new SavingsAccount();
+
+            decimal amountToDeposit = 100.00M;
+            decimal newBalance = checkingAccount.Deposit(amountToDeposit);
+
+            decimal amountToTransfer = 50.00M;
+            checkingAccount.Transfer(savingsAccount, amountToTransfer);
+
+            Console.WriteLine(checkingAccount.Balance);
+            Console.WriteLine(savingsAccount.Balance);
+
+            Console.ReadLine();
+
+            //BankAccount newBA = new BankAccount();
+            //BankAccount destinationAccount = new BankAccount();
+
+            //newBA.Deposit(100);
+
+            //newBA.Withdraw(50);
+            //newBA.Transfer(destinationAccount, 20);
+
+            //Console.WriteLine(newBA.Balance);
+            //Console.WriteLine(destinationAccount.Balance);
+
+
+            //Console.WriteLine("Hello World!");
+            //Console.ReadLine();
         }
     }
 }
