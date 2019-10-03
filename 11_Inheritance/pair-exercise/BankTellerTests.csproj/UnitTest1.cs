8d3ed33351be5f8eb54974fcbe50dbce8a031f60
Jules Kowalski <kowalski.jules@gmail.com>
2019-10-03 13:24:03 -0400

    Oct 3 1 pm

---
+++ 11_Inheritance/pair-exercise/BankTellerTests.csproj/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankTellerTests.csproj
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
