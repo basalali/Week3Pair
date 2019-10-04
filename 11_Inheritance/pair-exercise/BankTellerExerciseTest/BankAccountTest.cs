ebfd5eaa079884c35fa32f5de2fa8ef94fccf7f1
Basal Ali <basalali@hotmail.com>
2019-10-04 12:56:50 -0400

    Inheritance test complete

---
+++ 11_Inheritance/pair-exercise/BankTellerExerciseTest/BankAccountTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankTellerExercise.Tests
+{
+    [TestClass]
+    public class BankAccounTest
+    {
+        [TestMethod]
+        public void DepositTest()
+        {
+            BankAccount bankAccount = new BankAccount();
+
+            decimal result = bankAccount.Deposit(1000);
+
+            Assert.AreEqual(1000, result);
+
+            result = bankAccount.Deposit(0);
+
+            Assert.AreEqual(1000, result);
+            
+        }
+
+        [TestMethod]
+        public void WithdrawTest()
+        {
+            BankAccount bankAccount = new BankAccount();
+
+            decimal result = bankAccount.Deposit(1000);
+            result = bankAccount.Withdraw(500);
+
+            Assert.AreEqual(500, result);
+
+            result = bankAccount.Withdraw(250);
+
+            Assert.AreEqual(250, result);
+
+        }
+
+        [TestMethod]
+        public void TransferTest()
+        {
+            BankAccount  homeAccount = new BankAccount();
+            BankAccount destinationAccount = new BankAccount();
+
+           homeAccount.Deposit(1000);
+           homeAccount.Transfer(destinationAccount, 500);
+
+            decimal result = destinationAccount.Balance;
+
+            Assert.AreEqual(500, result);
+
+            
+
+
+
+
+
+        }
+
+
+    }
+}
