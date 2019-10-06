917e453e7de81cc66162c9456d6b646bdbf10c55
Basal Ali <basalali@hotmail.com>
2019-10-06 17:46:25 -0400

    salary worker test complete

---
+++ 12_Polymorphism/pair-exercise/Polymorphism.Tests/SalaryWorkerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmployeePayroll.CLasses
+{
+    [TestClass]
+    public class SalaryWorkTest
+    {
+        [TestMethod]
+        public void CalculateWeeklyPayTest()
+        {
+            SalaryWorker salariedMoney = new SalaryWorker(65000, "Basal", "Ali");
+
+            double actual = salariedMoney.CalculateWeeklyPay(60);
+            Assert.AreEqual(1250, actual);
+            // doesn't matter how many hour he/she works! their weekly pay is Annual Salary / 52 weeks!
+
+            actual = salariedMoney.CalculateWeeklyPay(900);
+            Assert.AreEqual(1250, actual);
+
+
+        }
+    }
+}
